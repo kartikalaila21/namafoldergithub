dd3439890bbb393e5d9fa1313f431fb2e3395113
kartikalaila21 <kartikalaila21@gmail.com>
2026-05-19 11:26:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,8 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
+
+import streamlit as st
+
+number = st.number_input("Insert a number")
+st.write("The current number is ", number)
