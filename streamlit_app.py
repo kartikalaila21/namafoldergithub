3a61f5fd8eef312145cab00216ad5cdd577cf9a7
kartikalaila21 <kartikalaila21@gmail.com>
2026-05-19 11:27:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,7 @@ st.markdown(multi)
 
 import streamlit as st
 
-number = st.number_input("Insert a number")
+number = st.number_input(
+    "Insert a number", value=None, placeholder="Type a number..."
+)
 st.write("The current number is ", number)
